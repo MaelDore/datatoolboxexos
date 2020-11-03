0b4e873ba3e7ae0afaefd350b49e35a598de6850
Mael DORE <mael.dore@gmail.com>
2020-11-03 14:32:45 +0100

    :panda_face: Turn into Research Compendium

---
+++ Prepare_Compendium.R
@@ -0,0 +1,5 @@
+library(rrtools)
+
+# Create a new Resarch Compendium project
+rrtools::use_compendium("../datatoolboxexos/",  # Use the ../ to get back to the parent folder and indicate the current working project that already exsits
+                        open = FALSE) # Do not open a new R Session
